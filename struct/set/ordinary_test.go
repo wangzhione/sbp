b77ee3d2b3badde3a50dcfb7a289590d943c535c
Zhi Wang <cSharpWZ@outlook.com>
2025-01-15 21:57:32 +0800

    add thread unsafe set

---
+++ struct/set/ordinary_test.go
@@ -0,0 +1,140 @@
+package set
+
+import (
+	"encoding/json"
+	"testing"
+)
+
+func TestThreadUnsafeSet_MarshalJSON(t *testing.T) {
+	expected := NewSetWithValue[int64](1, 2, 3)
+	var actual Set[int64]
+
+	// test Marshal from Set method
+	b, err := expected.MarshalJSON()
+	if err != nil {
+		t.Errorf("Error should be nil: %v", err)
+	}
+
+	err = json.Unmarshal(b, &actual)
+	if err != nil {
+		t.Errorf("Error should be nil: %v", err)
+	}
+
+	if !expected.EQual(actual) {
+		t.Errorf("Expected no difference, got: %v", expected.RemoveSet(actual))
+	}
+
+	// test Marshal from json package
+	b, err = json.Marshal(expected)
+	if err != nil {
+		t.Errorf("Error should be nil: %v", err)
+	}
+
+	err = json.Unmarshal(b, &actual)
+	if err != nil {
+		t.Errorf("Error should be nil: %v", err)
+	}
+
+	if !expected.EQual(actual) {
+		t.Errorf("Expected no difference, got: %v", expected.RemoveSet(actual))
+	}
+}
+
+func TestThreadUnsafeSet_UnmarshalJSON(t *testing.T) {
+	expected := NewSetWithValue[int64](1, 2, 3)
+	var actual Set[int64]
+
+	// test Unmarshal from Set method
+	err := actual.UnmarshalJSON([]byte(`[1, 2, 3]`))
+	if err != nil {
+		t.Errorf("Error should be nil: %v", err)
+	}
+	if !expected.EQual(actual) {
+		t.Errorf("Expected no difference, got: %v", expected.RemoveSet(actual))
+	}
+
+	// test Unmarshal from json package
+	actual = NewSet[int64]()
+	err = json.Unmarshal([]byte(`[1, 2, 3]`), &actual)
+	if err != nil {
+		t.Errorf("Error should be nil: %v", err)
+	}
+	if !expected.EQual(actual) {
+		t.Errorf("Expected no difference, got: %v", expected.RemoveSet(actual))
+	}
+}
+
+func TestThreadUnsafeSet_MarshalJSON_Struct(t *testing.T) {
+	expected := &testStruct{"test", NewSetWithValue("a")}
+
+	b, err := json.Marshal(&testStruct{"test", NewSetWithValue("a")})
+	if err != nil {
+		t.Errorf("Error should be nil: %v", err)
+	}
+
+	actual := &testStruct{}
+	err = json.Unmarshal(b, actual)
+	if err != nil {
+		t.Errorf("Error should be nil: %v", err)
+	}
+
+	if expected.Other != actual.Other || !expected.Set.EQual(actual.Set) {
+		t.Errorf("Expected no difference, got: %v", expected.Set.RemoveSet(actual.Set))
+	}
+}
+
+func TestThreadUnsafeSet_UnmarshalJSON_Struct(t *testing.T) {
+	expected := &testStruct{"test", NewSetWithValue("a", "b", "c")}
+	actual := &testStruct{}
+
+	err := json.Unmarshal([]byte(`{"other":"test", "set":["a", "b", "c"]}`), actual)
+	if err != nil {
+		t.Errorf("Error should be nil: %v", err)
+	}
+	if expected.Other != actual.Other || !expected.Set.EQual(actual.Set) {
+		t.Errorf("Expected no difference, got: %v", expected.Set.RemoveSet(actual.Set))
+	}
+
+	expectedComplex := NewSetWithValue(struct{ Val string }{Val: "a"}, struct{ Val string }{Val: "b"})
+	actualComplex := NewSet[struct{ Val string }]()
+
+	err = actualComplex.UnmarshalJSON([]byte(`[{"Val": "a"}, {"Val": "b"}]`))
+	if err != nil {
+		t.Errorf("Error should be nil: %v", err)
+	}
+	if !expectedComplex.EQual(actualComplex) {
+		t.Errorf("Expected no difference, got: %v", expectedComplex.RemoveSet(actualComplex))
+	}
+
+	actualComplex = NewSet[struct{ Val string }]()
+	err = json.Unmarshal([]byte(`[{"Val": "a"}, {"Val": "b"}]`), &actualComplex)
+	if err != nil {
+		t.Errorf("Error should be nil: %v", err)
+	}
+	if !expectedComplex.EQual(actualComplex) {
+		t.Errorf("Expected no difference, got: %v", expectedComplex.RemoveSet(actualComplex))
+	}
+}
+
+// this serves as an example of how to correctly unmarshal a struct with a Set property
+type testStruct struct {
+	Other string
+	Set   Set[string]
+}
+
+func (t *testStruct) UnmarshalJSON(b []byte) error {
+	raw := struct {
+		Other string
+		Set   []string
+	}{}
+
+	err := json.Unmarshal(b, &raw)
+	if err != nil {
+		return err
+	}
+
+	t.Other = raw.Other
+	t.Set = NewSetWithValue(raw.Set...)
+
+	return nil
+}
--- struct/set/set.go
+++ struct/set/set.go
@@ -1,101 +1,38 @@
 package set
 
+import (
+	"cmp"
+	"slices"
+)
+
 // Set is the primary interface provided by the mapset package.  It
 // represents an unordered set of data and a large number of
 // operations that can be applied to that set.
-type Set[T comparable] interface {
+type ISet[T comparable] interface {
 	// Add adds an element to the set.
-	Add(val T)
+	Add(v T)
 
 	// Append multiple elements to the set.
-	Append(val ...T)
+	Append(vals ...T)
 
 	// Len returns the number of elements in the set.
 	Len() int
 
-	// Contains returns whether the given items
+	// Exists returns whether the given items
 	// are all in the set.
-	Contains(val ...T) bool
+	Exists(vals ...T) bool
 
 	// Contain returns whether the given item
 	// is in the set.
 	//
-	// maybe Contains may cause the argument to escape to the heap.
-	Contain(val T) bool
+	// maybe Exists may cause the argument to escape to the heap.
+	Contain(v T) bool
 
-	// ContainSet returns whether at least one of the
-	// given element are in the set.
-	ContainSet(other Set[T]) bool
+	// Delete removes a single element from the set.
+	Delete(v T)
 
-	// Remove removes a single element from the set.
-	Remove(i T)
-
-	// Removes removes multiple elements from the set.
-	Removes(i ...T)
-
-	// Each iterates over elements and executes the passed func against each element.
-	// If passed func returns true, stop iteration at the time.
-	Each(func(T) bool)
-
-	// Equal determines if two sets are equal to each
-	// other. If they have the same cardinality
-	// and contain the same elements, they are
-	// considered equal. The order in which
-	// the elements were added is irrelevant.
-	//
-	// Note that the argument to Equal must be
-	// of the same type as the receiver of the
-	// method. Otherwise, Equal will panic.
-	Equal(other Set[T]) bool
-
-	// Clone returns a clone of the set using the same
-	// implementation, duplicating all keys.
-	Clone() Set[T]
-
-	// Difference returns the difference between this set
-	// and other. The returned set will contain
-	// all elements of this set that are not also
-	// elements of other.
-	//
-	// Note that the argument to Difference
-	// must be of the same type as the receiver
-	// of the method. Otherwise, Difference will
-	// panic.
-	Difference(other Set[T]) Set[T]
-
-	// Intersect returns a new set containing only the elements
-	// that exist only in both sets.
-	//
-	// Note that the argument to Intersect
-	// must be of the same type as the receiver
-	// of the method. Otherwise, Intersect will
-	// panic.
-	Intersect(other Set[T]) Set[T]
-
-	// IsSubSet determines if every element in this set is in
-	// the other set.
-	//
-	// Note that the argument to IsSubSet
-	// must be of the same type as the receiver
-	// of the method. Otherwise, IsSubSet will
-	// panic.
-	IsSubSet(other Set[T]) bool
-
-	// SymmetricDifference returns a new set with all elements which are
-	// in either this set or the other set but not in both.
-	//
-	// Note that the argument to SymmetricDifference
-	// must be of the same type as the receiver
-	// of the method. Otherwise, SymmetricDifference
-	// will panic.
-	SymmetricDifference(other Set[T]) Set[T]
-
-	// Union returns a new set with all elements in both sets.
-	//
-	// Note that the argument to Union must be of the
-	// same type as the receiver of the method.
-	// Otherwise, Union will panic.
-	Union(other Set[T]) Set[T]
+	// Remove removes multiple elements from the set.
+	Remove(vals ...T)
 
 	// ToSlice returns the members of the set as a slice.
 	ToSlice() []T
@@ -110,4 +47,12 @@ type Set[T comparable] interface {
 	// UnmarshalJSON will unmarshal a JSON-based byte slice into a full Set datastructure.
 	// For this to work, set subtypes must implemented the Marshal/Unmarshal interface.
 	UnmarshalJSON(b []byte) error
+}
+
+// Sorted returns a sorted slice of a set of any ordered type in ascending order.
+// When sorting floating-point numbers, NaNs are ordered before other values.
+func Sorted[T cmp.Ordered](s Set[T]) []T {
+	keys := s.ToSlice()
+	slices.Sort(keys)
+	return keys
 }
