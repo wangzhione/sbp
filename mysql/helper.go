0a31a13169a6e8eb7fb456dd56331c6d42251efe
Zhi Wang <cSharpWZ@outlook.com>
2025-01-27 19:25:36 +0800

    add mysql helper 驱动

--- mysql/helper.go
+++ mysql/helper.go
@@ -3,17 +3,22 @@ package mysql
 import (
 	"context"
 	"database/sql"
+	"fmt"
 	"log/slog"
+	"runtime/debug"
 	"sbp/util/trace"
 	"time"
 
 	_ "github.com/go-sql-driver/mysql" // init MySQL 驱动
 )
 
-type DB = *sql.DB
+// SQLHelper 数据库帮助结构体
+type SQLHelper struct {
+	DB *sql.DB
+}
 
 // NewMySQLHelper 创建一个新的 MySQLHelper 实例
-func NewMySQLHelper(ctx context.Context, config MySQLConfig) (db DB, err error) {
+func NewMySQLHelper(ctx context.Context, config MySQLConfig) (helper *SQLHelper, err error) {
 	// 构建 DSN（Data Source Name）
 	dsn := config.DataSourceName()
 	if trace.EnableLevel == slog.LevelDebug {
@@ -21,7 +26,7 @@ func NewMySQLHelper(ctx context.Context, config MySQLConfig) (db DB, err error) 
 	}
 
 	// 初始化数据库连接
-	db, err = sql.Open("mysql", dsn)
+	db, err := sql.Open("mysql", dsn)
 	if err != nil {
 		slog.ErrorContext(ctx, "failed to connect to MySQL", "dsn", dsn, "reason", err)
 		return
@@ -50,5 +55,126 @@ func NewMySQLHelper(ctx context.Context, config MySQLConfig) (db DB, err error) 
 	}
 
 	slog.InfoContext(ctx, "Connected to MySQL successfully!", "database", config.Database, "username", config.Username)
+	helper = &SQLHelper{DB: db}
 	return
+}
+
+// Close 关闭数据库连接
+func (helper *SQLHelper) Close() error {
+	return helper.DB.Close()
+}
+
+// Exec 执行无返回的 SQL 语句等 例如（INSERT, UPDATE, DELETE）
+func (helper *SQLHelper) Exec(ctx context.Context, query string, args ...any) (sql.Result, error) {
+	result, err := helper.DB.ExecContext(ctx, query, args...)
+	if err != nil {
+		slog.ErrorContext(ctx, "SQLHelper Exec error", "query", query, "args", args, "reason", err)
+	}
+	return result, err
+}
+
+// Query 执行查询，返回多行数据
+func (helper *SQLHelper) Query(ctx context.Context, query string, args ...any) (*sql.Rows, error) {
+	rows, err := helper.DB.QueryContext(ctx, query, args...)
+	if err != nil {
+		slog.ErrorContext(ctx, "SQLHelper Query error", "query", query, "args", args, "reason", err)
+	}
+	return rows, err
+}
+
+// QueryCallBack 执行查询, 内部自行通过闭包来完成参数传递和返回值获取
+// callback is for rows.Next() {}
+func (helper *SQLHelper) QueryCallBack(ctx context.Context, callback func(context.Context, *sql.Rows) error, query string, args ...any) error {
+	rows, err := helper.DB.QueryContext(ctx, query, args...)
+	if err != nil {
+		slog.ErrorContext(ctx, "SQLHelper QueryCallBack error", "query", query, "args", args, "reason", err)
+		return err
+	}
+	// recover panic and error rollback
+	defer func() {
+		if cover := recover(); cover != nil {
+			slog.ErrorContext(ctx, "SQLHelper QueryCallBack panic error", "recover", cover, "stack", debug.Stack())
+		}
+
+		newerr := rows.Close()
+		if newerr != nil {
+			slog.ErrorContext(ctx, "SQLHelper QueryCallBack rows.Close() panic error", "newerr", newerr)
+		}
+	}()
+
+	err = callback(ctx, rows)
+	if err != nil {
+		slog.ErrorContext(ctx, "SQLHelper QueryCallBack callback rows error", "query", query, "args", args, "reason", err)
+		return err
+	}
+
+	err = rows.Err()
+	if err != nil {
+		slog.ErrorContext(ctx, "SQLHelper QueryCallBack rows.Err() error", "query", query, "args", args, "reason", err)
+		return err
+	}
+
+	return nil
+}
+
+/*
+ QueryRow template
+
+	err := helper.DB.QueryRowContext(ctx, query, args...).Scan(dest...)
+	switch err {
+	case nil:
+		// success
+		return nil;
+	case sql.ErrNoRows: // 没有记录，返回空值
+		slog.InfoContext(ctx, "SQLHelper QueryRow sql.ErrNoRows")
+		return err; // or empty 业务逻辑处理
+	default:
+		slog.ErrorContext(ctx, "SQLHelper QueryRow error", "query", query, "args", args, "reason", err)
+		return nil;
+	}
+*/
+
+// BeginTransaction 开启事务
+func (helper *SQLHelper) BeginTransaction(ctx context.Context, transaction func(context.Context, *sql.Tx) error) (err error) {
+	// opts *sql.TxOptions 用于指定事务的隔离级别和是否为只读事务。可选参数，可以传 nil 使用 mysql 默认配置。
+	tx, err := helper.DB.BeginTx(ctx, nil)
+	if err != nil {
+		slog.ErrorContext(ctx, "SQLHelper BeginTransaction error", "reason", err)
+		return err
+	}
+
+	// recover panic and error rollback
+	defer func() {
+		if cover := recover(); cover != nil {
+			slog.ErrorContext(ctx, "SQLHelper BeginTransaction panic error", "recover", cover, "stack", debug.Stack())
+
+			newerr := tx.Rollback()
+			if newerr != nil && newerr != sql.ErrTxDone {
+				slog.ErrorContext(ctx, "SQLHelper BeginTransaction Rollback defer panic error", "newerr", newerr)
+			}
+
+			err = fmt.Errorf("transaction panic: %v", cover)
+			return
+		}
+	}()
+
+	err = transaction(ctx, tx)
+	if err != nil {
+		slog.ErrorContext(ctx, "SQLHelper BeginTransaction transaction error", "reason", err)
+
+		newerr := tx.Rollback()
+		if newerr != nil && newerr != sql.ErrTxDone {
+			// 依赖人工每日接入, 追查细节
+			slog.ErrorContext(ctx, "SQLHelper BeginTransaction Rollback panic error", "newerr", newerr)
+		}
+		return err
+	}
+
+	// transaction success commit
+	err = tx.Commit()
+	if err != nil && err != sql.ErrTxDone {
+		slog.ErrorContext(ctx, "SQLHelper BeginTransaction Commit panic error", "newerr", err)
+	}
+
+	return nil
 }
