9aa91cf477658db4828bb17cb0e764030e07245c
Zhi Wang <cSharpWZ@outlook.com>
2025-02-05 15:28:05 +0800

    backward.go 修复

--- cloud/metainfo/backward.go
+++ cloud/metainfo/backward.go
@@ -25,38 +25,54 @@ func newBackwardCtxValues() *bwCtxValue {
 
 func (p *bwCtxValue) get(k string) (v string, ok bool) {
 	p.RLock()
+	defer p.RUnlock()
+
 	v, ok = p.kvs[k]
-	p.RUnlock()
 	return
 }
 
 func (p *bwCtxValue) getAll() (m map[string]string) {
 	p.RLock()
-	if cnt := len(p.kvs); cnt > 0 {
-		m = make(map[string]string, cnt)
+	defer p.RUnlock()
+
+	if len(p.kvs) > 0 {
+		m = make(map[string]string, len(p.kvs))
 		for k, v := range p.kvs {
 			m[k] = v
 		}
 	}
-	p.RUnlock()
 	return
 }
 
 func (p *bwCtxValue) set(k, v string) {
 	p.Lock()
+	defer p.Unlock()
+
 	p.kvs[k] = v
-	p.Unlock()
 }
 
-func (p *bwCtxValue) setMany(kvs []string) {
+// setMany len(kvs) 必须是偶数
+func (p *bwCtxValue) setMany(kvs []string) bool {
+	if len(kvs) == 0 {
+		return true
+	}
+	if len(kvs)%2 != 0 {
+		return false
+	}
+
 	p.Lock()
 	for i := 0; i < len(kvs); i += 2 {
 		p.kvs[kvs[i]] = kvs[i+1]
 	}
 	p.Unlock()
+	return true
 }
 
 func (p *bwCtxValue) setMap(kvs map[string]string) {
+	if len(kvs) == 0 {
+		return
+	}
+
 	p.Lock()
 	for k, v := range kvs {
 		p.kvs[k] = v
@@ -70,8 +86,7 @@ func WithBackwardValues(ctx context.Context) context.Context {
 	if _, ok := ctx.Value(bwCtxKeyRecv).(*bwCtxValue); ok {
 		return ctx
 	}
-	ctx = context.WithValue(ctx, bwCtxKeyRecv, newBackwardCtxValues())
-	return ctx
+	return context.WithValue(ctx, bwCtxKeyRecv, newBackwardCtxValues())
 }
 
 // RecvBackwardValue gets a value associated with the given key that is set by
@@ -92,7 +107,7 @@ func RecvAllBackwardValues(ctx context.Context) (m map[string]string) {
 }
 
 // SetBackwardValue sets a key value pair into the context.
-func SetBackwardValue(ctx context.Context, key, val string) (ok bool) {
+func SetBackwardValue(ctx context.Context, key, val string) bool {
 	if p, ok := ctx.Value(bwCtxKeyRecv).(*bwCtxValue); ok {
 		p.set(key, val)
 		return true
@@ -101,25 +116,20 @@ func SetBackwardValue(ctx context.Context, key, val string) (ok bool) {
 }
 
 // SetBackwardValues is the batched version of `SetBackwardValue`.
-func SetBackwardValues(ctx context.Context, kvs ...string) (ok bool) {
-	if l := len(kvs); l > 0 && l%2 == 0 {
-		if p, ok := ctx.Value(bwCtxKeyRecv).(*bwCtxValue); ok {
-			p.setMany(kvs)
-			return true
-		}
+func SetBackwardValues(ctx context.Context, kvs ...string) bool {
+	if p, ok := ctx.Value(bwCtxKeyRecv).(*bwCtxValue); ok {
+		return p.setMany(kvs)
 	}
-	return
+	return false
 }
 
 // SetBackwardValuesFromMap is the batched version of `SetBackwardValue`.
-func SetBackwardValuesFromMap(ctx context.Context, kvs map[string]string) (ok bool) {
-	if l := len(kvs); l > 0 {
-		if p, ok := ctx.Value(bwCtxKeyRecv).(*bwCtxValue); ok {
-			p.setMap(kvs)
-			return true
-		}
+func SetBackwardValuesFromMap(ctx context.Context, kvs map[string]string) bool {
+	if p, ok := ctx.Value(bwCtxKeyRecv).(*bwCtxValue); ok {
+		p.setMap(kvs)
+		return true
 	}
-	return
+	return false
 }
 
 // WithBackwardValuesToSend returns a new context that collects key-value
@@ -129,49 +139,43 @@ func WithBackwardValuesToSend(ctx context.Context) context.Context {
 	if _, ok := ctx.Value(bwCtxKeySend).(*bwCtxValue); ok {
 		return ctx
 	}
-	ctx = context.WithValue(ctx, bwCtxKeySend, newBackwardCtxValues())
-	return ctx
+	return context.WithValue(ctx, bwCtxKeySend, newBackwardCtxValues())
 }
 
 // SendBackwardValue sets a key-value pair into the context for sending to
 // a remote endpoint.
 // Note that the values can not be retrieved with `RecvBackwardValue` from
 // the same context.
-func SendBackwardValue(ctx context.Context, key, val string) (ok bool) {
+func SendBackwardValue(ctx context.Context, key, val string) bool {
 	if p, ok := ctx.Value(bwCtxKeySend).(*bwCtxValue); ok {
 		p.set(key, val)
 		return true
 	}
-	return
+	return false
 }
 
 // SendBackwardValues is the batched version of `SendBackwardValue`.
-func SendBackwardValues(ctx context.Context, kvs ...string) (ok bool) {
-	if l := len(kvs); l > 0 && l%2 == 0 {
-		if p, ok := ctx.Value(bwCtxKeySend).(*bwCtxValue); ok {
-			p.setMany(kvs)
-			return true
-		}
+func SendBackwardValues(ctx context.Context, kvs ...string) bool {
+	if p, ok := ctx.Value(bwCtxKeySend).(*bwCtxValue); ok {
+		return p.setMany(kvs)
 	}
-	return
+	return false
 }
 
 // SendBackwardValuesFromMap is the batched version of `SendBackwardValue`.
-func SendBackwardValuesFromMap(ctx context.Context, kvs map[string]string) (ok bool) {
-	if l := len(kvs); l > 0 {
-		if p, ok := ctx.Value(bwCtxKeySend).(*bwCtxValue); ok {
-			p.setMap(kvs)
-			return true
-		}
+func SendBackwardValuesFromMap(ctx context.Context, kvs map[string]string) bool {
+	if p, ok := ctx.Value(bwCtxKeySend).(*bwCtxValue); ok {
+		p.setMap(kvs)
+		return true
 	}
-	return
+	return false
 }
 
 // GetBackwardValueToSend gets a value associated with the given key that is set by
 // `SendBackwardValue`, `SendBackwardValues` or `SendBackwardValuesFromMap`.
 func GetBackwardValueToSend(ctx context.Context, key string) (val string, ok bool) {
 	if p, exist := ctx.Value(bwCtxKeySend).(*bwCtxValue); exist {
-		val, ok = p.get(key)
+		return p.get(key)
 	}
 	return
 }
@@ -184,16 +188,4 @@ func AllBackwardValuesToSend(ctx context.Context) (m map[string]string) {
 		return p.getAll()
 	}
 	return
-}
-
-// GetBackwardValue is an alias to `RecvBackwardValue`.
-// Deprecated: for clarity, please use `RecvBackwardValue` instead.
-func GetBackwardValue(ctx context.Context, key string) (val string, ok bool) {
-	return RecvBackwardValue(ctx, key)
-}
-
-// GetAllBackwardValues is an alias to RecvAllBackwardValues.
-// Deprecated: for clarity, please use `RecvAllBackwardValues` instead.
-func GetAllBackwardValues(ctx context.Context) map[string]string {
-	return RecvAllBackwardValues(ctx)
 }
--- cloud/metainfo/backward_test.go
+++ cloud/metainfo/backward_test.go
@@ -2,41 +2,8 @@ package metainfo
 
 import (
 	"context"
-	"fmt"
 	"testing"
 )
-
-func calls(ctx context.Context, level int, t *testing.T, expect bool) {
-	k := fmt.Sprintf("key-%d", level)
-	v := fmt.Sprintf("val-%d", level)
-	b := SetBackwardValue(ctx, k, v)
-	assert(t, expect == b, "expect", expect, "got", b)
-
-	if level > 0 {
-		calls(ctx, level-1, t, expect)
-	}
-}
-
-func TestWithBackwardValues(t *testing.T) {
-	ctx := context.Background()
-
-	ctx = WithBackwardValues(ctx)
-	calls(ctx, 2, t, true)
-
-	m := GetAllBackwardValues(ctx)
-	assert(t, len(m) == 3)
-	assert(t, m["key-0"] == "val-0")
-	assert(t, m["key-1"] == "val-1")
-	assert(t, m["key-2"] == "val-2")
-}
-
-func TestWithBackwardValues2(t *testing.T) {
-	ctx := context.Background()
-	calls(ctx, 2, t, false)
-
-	m := GetAllBackwardValues(ctx)
-	assert(t, len(m) == 0)
-}
 
 func TestWithBackwardValues3(t *testing.T) {
 	ctx0 := context.Background()
@@ -51,8 +18,9 @@ func TestWithBackwardValues4(t *testing.T) {
 	ctx1 := WithBackwardValues(ctx0)
 	ctx2 := WithValue(ctx1, "key", "forward")
 
-	val, ok := GetBackwardValue(ctx0, "key")
+	val, ok := RecvBackwardValue(ctx0, "key")
 	assert(t, !ok)
+	assert(t, val == "")
 
 	ok = SetBackwardValue(ctx2, "key", "backward")
 	assert(t, ok)
@@ -61,11 +29,11 @@ func TestWithBackwardValues4(t *testing.T) {
 	assert(t, ok)
 	assert(t, val == "forward")
 
-	val, ok = GetBackwardValue(ctx2, "key")
+	val, ok = RecvBackwardValue(ctx2, "key")
 	assert(t, ok)
 	assert(t, val == "backward")
 
-	val, ok = GetBackwardValue(ctx1, "key")
+	val, ok = RecvBackwardValue(ctx1, "key")
 	assert(t, ok)
 	assert(t, val == "backward")
 
@@ -75,14 +43,14 @@ func TestWithBackwardValues4(t *testing.T) {
 	assert(t, ok)
 	assert(t, val == "forward")
 
-	val, ok = GetBackwardValue(ctx3, "key")
+	val, ok = RecvBackwardValue(ctx3, "key")
 	assert(t, ok)
 	assert(t, val == "backward")
 
 	ok = SetBackwardValue(ctx3, "key", "backward2")
 	assert(t, ok)
 
-	val, ok = GetBackwardValue(ctx1, "key")
+	val, ok = RecvBackwardValue(ctx1, "key")
 	assert(t, ok)
 	assert(t, val == "backward2")
 }
