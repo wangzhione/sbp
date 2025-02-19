692c04f108b67b2d01489ef44c95793a84e25543
zhi.wang <zhi.wang@kunlun-inc.com>
2025-02-19 18:23:59 +0800

    go ip 库补充

--- go.mod
+++ go.mod
@@ -6,7 +6,7 @@ require github.com/google/uuid v1.6.1-0.20241114170450-2d3c2a9cc518
 
 require (
 	github.com/cespare/xxhash/v2 v2.3.1-0.20240703180136-ab37246c889f
-	github.com/go-sql-driver/mysql v1.8.1-0.20250123093231-85c6311943c8
+	github.com/go-sql-driver/mysql v1.9.0
 	github.com/spf13/cast v1.7.1
 	github.com/stretchr/testify v1.10.1-0.20241221131041-7c367bb7bc7d
 )
--- util/httpip/ip.go
+++ util/httpip/ip.go
@@ -1,6 +1,7 @@
 package httpip
 
 import (
+	"io"
 	"net"
 	"net/http"
 	"strings"
@@ -38,4 +39,108 @@ func GetClientIP(r *http.Request) (ip string) {
 	// 兜底直接使用 client 请求的 ip 地址
 	ip, _, _ = net.SplitHostPort(r.RemoteAddr)
 	return
+}
+
+// GetMACAddress 获取本机 MAC 地址
+func GetMACAddress() (macList []string, err error) {
+	interfaces, err := net.Interfaces()
+	if err != nil {
+		return
+	}
+
+	for _, i := range interfaces {
+		if i.HardwareAddr != nil {
+			macList = append(macList, i.HardwareAddr.String())
+		}
+	}
+	return
+}
+
+// GetAllIP 获取本机所有 IP 地址（包括本地和外部）
+func GetAllIP(includeIPv6 ...struct{}) ([]string, error) {
+	var ipList []string
+
+	interfaces, err := net.Interfaces()
+	if err != nil {
+		return nil, err
+	}
+
+	for _, i := range interfaces {
+		addrs, err := i.Addrs()
+		if err != nil {
+			continue
+		}
+
+		for _, addr := range addrs {
+			if ipNet, ok := addr.(*net.IPNet); ok {
+				if len(includeIPv6) == 0 && ipNet.IP.To4() == nil {
+					continue
+				}
+				ipList = append(ipList, ipNet.IP.String())
+			}
+		}
+	}
+	return ipList, nil
+}
+
+// GetExternalIP 获取本机外部 IP 地址（排除 127.0.0.1）
+func GetExternalIP(includeIPv6 ...struct{}) ([]string, error) {
+	var ipList []string
+
+	// 在某些操作系统（如 Linux 或 MacOS）上，获取某些网络接口的地址可能需要管理员权限
+	interfaces, err := net.Interfaces()
+	if err != nil {
+		return nil, err
+	}
+
+	for _, i := range interfaces {
+		addrs, err := i.Addrs()
+		if err != nil {
+			continue
+		}
+
+		for _, addr := range addrs {
+			ipNet, ok := addr.(*net.IPNet)
+			if ok && !ipNet.IP.IsLoopback() && (len(includeIPv6) == 0 || ipNet.IP.To4() != nil) {
+				ipList = append(ipList, ipNet.IP.String())
+			}
+		}
+	}
+	return ipList, nil
+}
+
+// GetPublicIP 获取本机公网 IP 地址
+func GetPublicIP() (string, error) {
+	// 公网 IP 查询服务
+	resp, err := http.Get("https://api64.ipify.org?format=text")
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	ip, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	return string(ip), nil
+}
+
+var privateBlocks = []string{
+	"10.",      // 10.0.0.0 – 10.255.255.255
+	"172.16.",  // 172.16.0.0 – 172.31.255.255
+	"192.168.", // 192.168.0.0 – 192.168.255.255
+	"127.",     // Loopback
+	"fc00:",    // IPv6 Unique Local Address
+	"fe80:",    // IPv6 Link-Local Address
+}
+
+// IsPrivateIP 检查 IP 地址是否为内网地址
+func IsPrivateIP(ip string) bool {
+	for _, block := range privateBlocks {
+		if strings.HasPrefix(ip, block) {
+			return true
+		}
+	}
+	return false
 }
