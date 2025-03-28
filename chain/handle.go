3cc790ecf76538da27eef8a1bede569bddef086d
zhi.wang <zhi.wang@kunlun-inc.com>
2025-03-28 18:26:53 +0800

    slog 精简

--- chain/handle.go
+++ chain/handle.go
@@ -12,6 +12,7 @@ type TraceHandler struct {
 	slog.Handler
 }
 
+// Handle add trace id @see https://github.com/golang/go/issues/73054#event-16988835247
 func (h TraceHandler) Handle(ctx context.Context, r slog.Record) error {
 	fs := runtime.CallersFrames([]uintptr{r.PC})
 	f, _ := fs.Next()
@@ -23,34 +24,10 @@ func (h TraceHandler) Handle(ctx context.Context, r slog.Record) error {
 	funcName := f.Function[i+1:]
 	sourceValue := fmt.Sprintf("%s:%d %s", filepath.Base(f.File), f.Line, funcName)
 	// add short source
-	source := slog.String(slog.SourceKey, sourceValue)
+	r.AddAttrs(slog.String(slog.SourceKey, sourceValue))
 
 	// context 依赖 WithContext(ctx, id) or Request(r)
-	trace := slog.String(XRquestID, GetTraceID(ctx))
+	r.AddAttrs(slog.String(XRquestID, GetTraceID(ctx)))
 
-	// @see https://github.com/golang/go/issues/73054#event-16988835247
-	// // Unsafe code access to internal fields of slog.Record
-	// rPtr := unsafe.Pointer(&r)
-	// rv := reflect.NewAt(reflect.TypeOf(r), rPtr).Elem()
-	//
-	// frontField := rv.FieldByIndex([]int{4})
-	// front := *(*[5]slog.Attr)(unsafe.Pointer(frontField.UnsafeAddr()))
-	//
-	// nFrontField := rv.FieldByIndex([]int{5})
-	// nFrontPtr := (*int)(unsafe.Pointer(nFrontField.UnsafeAddr()))
-	//
-	// backField := rv.FieldByIndex([]int{6})
-	// backPtr := (*[]slog.Attr)(unsafe.Pointer(backField.UnsafeAddr()))
-	//
-	// newback := []slog.Attr{source, trace}
-	// for i := range *nFrontPtr {
-	// 	newback = append(newback, front[i])
-	// }
-	// *(*int)(nFrontPtr) = 0
-	//
-	// newback = append(newback, *backPtr...)
-	// *(*[]slog.Attr)(backPtr) = newback
-
-	r.AddAttrs(source, trace)
 	return h.Handler.Handle(ctx, r)
 }
