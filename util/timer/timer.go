0cb85d5985530f5d02c2c43544fd8f4dfff8f39a
zhi.wang <csharpwz@outlook.com>
2025-03-28 17:43:23 +0800

    add timer

--- util/timer/timer.go
+++ util/timer/timer.go
@@ -2,6 +2,9 @@ package timer
 
 import "time"
 
+// 要求 go version >= 1.23
+// time.Timer 1.23 之后语义发生变化, 修复 1.23 之前啰嗦 stop select case 写法
+
 // Don’t pool what you don’t need to. GC is cheap. Your code’s clarity is expensive.
 // 官方 net/http 标准库也没使用 timer pool（他们是高性能场景，虽然不极端）
 
@@ -16,17 +19,4 @@ func NewTimer(d time.Duration) *Timer {
 
 func (t *Timer) Reset() {
 	t.Timer.Reset(t.Life)
-}
-
-// time.Timer 1.23 之后语义发生变化, 修复 1.23 之前啰嗦 stop select case 写法
-
-func StopTimer(t *time.Timer) { // Deprecated go 1.23 之前用法
-	// For a Timer created with NewTimer,
-	// Reset should be invoked only on stopped or expired timers with drained channels.
-	if !t.Stop() {
-		select {
-		case <-t.C: // try to drain the channel
-		default:
-		}
-	}
 }
