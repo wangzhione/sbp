52280c655c6fb8591b42b4341d24680e188d493f
zhi.wang <zhi.wang@kunlun-inc.com>
2025-09-08 16:18:29 +0800

    httpserve update

--- system/version.go
+++ system/version.go
@@ -5,6 +5,29 @@ import (
 	"runtime/debug"
 )
 
+// Linux 默认是服务部署的最终服务器, 方便利用 system.Linux 默认做一些特殊处理逻辑
+const Linux bool = runtime.GOOS == "linux"
+
+/*
+ runtime.GOOS 是 Go 语言中的一个常量，用于获取当前操作系统的名称。它的枚举值包括但不限于：
+
+ windows
+ linux
+ darwin (macOS)
+ freebsd
+ openbsd
+ netbsd
+ android
+ ios
+ js (用于 Go 编译为 JavaScript)
+ plan9
+ solaris
+*/
+
+const Windows bool = runtime.GOOS == "windows"
+
+const Darwin bool = runtime.GOOS == "darwin"
+
 // BuildVersion 完整编译版本
 var BuildVersion string = runtime.Version()
 
