a044b6fefaae94f489ec55cb55f64a834d12176f
zhi.wang <zhi.wang@kunlun-inc.com>
2025-10-16 10:50:09 +0800

    重新调整 id hash 签名

--- util/idhash/md5.go
+++ util/idhash/md5.go
@@ -9,13 +9,9 @@ import (
 )
 
 // MD5 string to md5 sign
-func MD5(s string) string {
-	return MD5Data([]byte(s))
-}
-
-func MD5Data(data []byte) string {
+func MD5[T ~[]byte | ~string](data T) string {
 	// 小写 16 进制
-	digest := md5.Sum(data)
+	digest := md5.Sum([]byte(data))
 	return hex.EncodeToString(digest[:])
 }
 
--- util/idhash/xxhash.go
+++ util/idhash/xxhash.go
@@ -3,12 +3,7 @@ package idhash
 import "github.com/cespare/xxhash/v2"
 
 // Hash returns the hash value of the byte slice in 64bits.
-func Hash(data []byte) uint64 {
-	return xxhash.Sum64(data)
-}
-
-// HashString returns the hash value of the string in 64bits.
-func HashString(s string) uint64 {
-	// As of go 1.22, string to bytes conversion []bytes(str) is faster than using the unsafe package.
-	return Hash([]byte(s))
+// Hash String returns the hash value of the string in 64bits.
+func Hash[T ~[]byte | ~string](data T) uint64 {
+	return xxhash.Sum64([]byte(data))
 }
